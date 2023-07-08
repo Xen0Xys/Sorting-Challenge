e89ba2e3c9e88df94a0ef1deb75a1495daa64b1b
Czekaj Tom <red57101@gmail.com>
2023-07-09 01:07:01 +0200

    :rocket: Add main class

--- src/main/java/fr/xen0xys/Main.java
+++ src/main/java/fr/xen0xys/Main.java
@@ -1,7 +1,9 @@
 package fr.xen0xys;
 
+import fr.xen0xys.ui.GameWindow;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        GameWindow gameWindow = new GameWindow();
     }
 }
